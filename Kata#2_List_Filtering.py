f94efbbb00e413a9fa6e1478ee565c7b7ea666fe
cuxhausen <cuxhausen@mail.ru>
2021-11-22 21:42:55 +0600

    Changed file Kata#2

--- Kata#2_List_Filtering.py
+++ Kata#2_List_Filtering.py
@@ -11,23 +11,31 @@ filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123]"""
 
 # My solutions
+
 l = [1,2,'a','b']
 def filter_list(l):
     q = [e for e in l if isinstance(e, int)]
     return q
 
-# Other solutions
+# The best solution
+
 def filter_list(l):
   'return a new list with the strings filtered out'
   return [i for i in l if not isinstance(i, str)]
+
+# Other solutions
 
 def filter_list(l):
   'return a new list with the strings filtered out'
   return [x for x in l if type(x) is not str]
 
+##################################################
+
 def filter_list(l):
   'return a new list with the strings filtered out'
   return [e for e in l if isinstance(e, int)]
+
+##################################################
 
 def filter_list(l):
     new_list =[]
@@ -36,8 +44,12 @@ def filter_list(l):
             new_list.append(x)
     return new_list
 
+##################################################
+
 def filter_list(l):
     return filter(lambda x: not type(x) is str, l)
+
+###################################################
 
 def filter_list(l):
   'return a new list with the strings filtered out'
