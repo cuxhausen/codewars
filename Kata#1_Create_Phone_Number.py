f16ab1613afdd04140578404a92b764ea3d7bb7c
cuxhausen <cuxhausen@mail.ru>
2021-11-22 21:37:25 +0600

    Changed file Kata#1

--- Kata#1_Create_Phone_Number.py
+++ Kata#1_Create_Phone_Number.py
@@ -11,6 +11,7 @@ The returned format must be correct in order to complete this challenge.
 Don't forget the space after the closing parentheses!"""
 
 # My solutions
+
 def create_phone_number(n):
     n.insert(0, '(')
     n.insert(4, ') ')
@@ -20,14 +21,18 @@ def create_phone_number(n):
         a += str(i)
     return a
 
-# Other solutions
+# The best solution
 
 def create_phone_number(n):
     return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
 
+# Other solutions
+
 def create_phone_number(n):
     n = ''.join(map(str,n))
     return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
+
+###############################################
 
 def create_phone_number(n):
     str1 =  ''.join(str(x) for x in n[0:3])
@@ -35,18 +40,28 @@ def create_phone_number(n):
     str3 =  ''.join(str(x) for x in n[6:10])
     return '({}) {}-{}'.format(str1, str2, str3)
 
+###############################################
+
 def create_phone_number(n):
     n = "".join([str(x) for x in n] )
     return("(" + str(n[0:3]) + ")" + " " + str(n[3:6]) + "-" + str(n[6:]))
+
+##############################################
 
 def create_phone_number(n):
     m = ''.join(map(str, n))
     return f"({m[:3]}) {m[3:6]}-{m[6:]}"
 
+##############################################
+
 def create_phone_number(n):
     return "(%i%i%i) %i%i%i-%i%i%i%i" % tuple(n)
 
+##############################################
+
 create_phone_number = lambda n: f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
+
+##############################################
 
 def create_phone_number(n):
   d='('
